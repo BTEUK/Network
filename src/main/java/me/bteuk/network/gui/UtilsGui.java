432d9453c8b7c5c98c4aadaaf074e77f8aab53f7
ELgamer <laurens.wolfert@gmail.com>
2023-08-19 12:21:05 +0200

    BTEUK-291: Added light and barrier blocks to the utils menu. Added permissions to enable/disable.

--- src/main/java/me/bteuk/network/gui/UtilsGui.java
+++ src/main/java/me/bteuk/network/gui/UtilsGui.java
@@ -43,6 +43,54 @@ public class UtilsGui extends Gui {
 
                 });
 
+        //Get light block.
+        setItem(11, Utils.createItem(Material.LIGHT, 1,
+                        Utils.title("Light Block"),
+                        Utils.line("Click to get a light block.")),
+                u ->
+
+                {
+
+                    //Delete this gui.
+                    u.player.closeInventory();
+
+                    //Check if the user has permission.
+                    if (!u.player.hasPermission("uknet.lightblock")) {
+
+                        u.player.sendMessage(Utils.error("You do not have permission to use this command."));
+                        return;
+
+                    }
+
+                    //Add debug stick to inventory.
+                    Utils.giveItem(u.player, new ItemStack(Material.LIGHT), "Light");
+
+                });
+
+        //Get barrier block.
+        setItem(15, Utils.createItem(Material.BARRIER, 1,
+                        Utils.title("Barrier"),
+                        Utils.line("Click to get a barrier.")),
+                u ->
+
+                {
+
+                    //Delete this gui.
+                    u.player.closeInventory();
+
+                    //Check if the user has permission.
+                    if (!u.player.hasPermission("uknet.barrier")) {
+
+                        u.player.sendMessage(Utils.error("You do not have permission to use this command."));
+                        return;
+
+                    }
+
+                    //Add debug stick to inventory.
+                    Utils.giveItem(u.player, new ItemStack(Material.BARRIER), "Barrier");
+
+                });
+
         //Return
         setItem(26, Utils.createItem(Material.SPRUCE_DOOR, 1,
                         Utils.title("Return"),
