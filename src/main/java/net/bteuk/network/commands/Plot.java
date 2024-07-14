dfd08e94e4517499e38b095b6645bef86e6d776d
ELgamer <laurens.wolfert@gmail.com>
2024-07-14 11:04:37 +0200

    Open plot feedback with /plot feedback <plotID>
    - Only for a plot you are currently the owner or member of.

--- src/main/java/net/bteuk/network/commands/Plot.java
+++ src/main/java/net/bteuk/network/commands/Plot.java
@@ -8,12 +8,18 @@ import net.bteuk.network.gui.plotsystem.PlotMenu;
 import net.bteuk.network.lib.utils.ChatUtils;
 import net.bteuk.network.sql.PlotSQL;
 import net.bteuk.network.utils.NetworkUser;
+import net.bteuk.network.utils.Utils;
 import net.bteuk.network.utils.enums.PlotStatus;
+import net.kyori.adventure.inventory.Book;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static net.bteuk.network.utils.Constants.LOGGER;
@@ -58,6 +64,7 @@ public class Plot extends AbstractCommand {
             case "menu" -> menu(p);
             case "info" -> info(p, plotID);
             case "join" -> join(p, plotID);
+            case "feedback" -> feedback(p, plotID);
             default -> error(p);
         }
         return true;
@@ -125,9 +132,59 @@ public class Plot extends AbstractCommand {
         }
     }
 
+    private void feedback(Player player, int plot) {
+        if (plot == -1) {
+            error(player);
+            return;
+        }
+        // Check if the player is the owner of a member of the plot.
+        // Then open the latest feedback.
+        // And set their Main gui to the plot info of this plot.
+        if (!plotSQL.hasRow("SELECT id FROM plot_members WHERE id=" + plot + " AND uuid='" + player.getUniqueId() + "';")) {
+            player.sendMessage(ChatUtils.error("You are no longer the owner or a member of this plot."));
+            return;
+        }
+
+        // Find the latest attempt.
+        String uuid = plotSQL.getString("SELECT uuid FROM plot_members WHERE id=" + plot + " AND is_owner=1;");
+        int latestAttempt = plotSQL.getInt("SELECT MAX(attempt) FROM deny_data WHERE id=" + plot + " AND uuid='" + uuid + "';");
+
+        if (latestAttempt == 0) {
+            player.sendMessage(Utils.error("There is no feedback available for this plot."));
+            return;
+        }
+
+        NetworkUser user = instance.getUser(player);
+        if (user != null) {
+            user.mainGui = new PlotInfo(user, plot);
+        }
+
+        // Open the feedback, if it exists.
+        //Create book.
+        Component title = Component.text("Plot " + plot + " Attempt " + latestAttempt, NamedTextColor.AQUA, TextDecoration.BOLD);
+        Component author = Component.text(instance.getGlobalSQL().getString("SELECT name FROM player_data WHERE uuid='" +
+                plotSQL.getString("SELECT reviewer FROM deny_data WHERE id=" + plot + " AND uuid='" + uuid + "' AND attempt=" + latestAttempt + ";") + "';"));
+
+        //Get pages of the book.
+        ArrayList<String> sPages = plotSQL.getStringList("SELECT contents FROM book_data WHERE id="
+                + plotSQL.getInt("SELECT book_id FROM deny_data WHERE id=" + plot + " AND uuid='" + uuid + "' AND attempt=" + latestAttempt + ";") + ";");
+
+        //Create a list of components from the list of strings.
+        ArrayList<Component> pages = new ArrayList<>();
+        for (String page : sPages) {
+            pages.add(Component.text(page));
+        }
+
+        Book book = Book.book(title, author, pages);
+
+        //Open the book.
+        player.openBook(book);
+    }
+
     private void error(Player p) {
         p.sendMessage(ChatUtils.error("/plot menu"));
         p.sendMessage(ChatUtils.error("/plot info <plotID>"));
         p.sendMessage(ChatUtils.error("/plot join <plotID>"));
+        p.sendMessage(ChatUtils.error("/plot feedback <plotID>"));
     }
 }
