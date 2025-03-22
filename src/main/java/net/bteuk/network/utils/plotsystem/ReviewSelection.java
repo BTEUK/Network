859a46937ca6c136a13093cabb0851b65081ed73
ELgamer <laurens.wolfert@gmail.com>
2025-03-22 19:36:44 +0100

    Add hover text to ReviewSelection

--- src/main/java/net/bteuk/network/utils/plotsystem/ReviewSelection.java
+++ src/main/java/net/bteuk/network/utils/plotsystem/ReviewSelection.java
@@ -2,13 +2,14 @@ package net.bteuk.network.utils.plotsystem;
 
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 @Getter
 public enum ReviewSelection {
-    GOOD(Component.text("[Good]", NamedTextColor.GREEN), Component.text("[G]", NamedTextColor.GREEN)),
-    OK(Component.text("[Ok]", NamedTextColor.YELLOW), Component.text("[O]", NamedTextColor.YELLOW)),
-    POOR(Component.text("[Poor]", NamedTextColor.RED), Component.text("[P]", NamedTextColor.RED)),
+    GOOD(Component.text("[Good]", NamedTextColor.GREEN), Component.text("[G]", NamedTextColor.GREEN).hoverEvent(HoverEvent.showText(Component.text("Good")))),
+    OK(Component.text("[Ok]", NamedTextColor.YELLOW), Component.text("[O]", NamedTextColor.GOLD).hoverEvent(HoverEvent.showText(Component.text("Ok")))),
+    POOR(Component.text("[Poor]", NamedTextColor.RED), Component.text("[P]", NamedTextColor.RED).hoverEvent(HoverEvent.showText(Component.text("Poor")))),
     NONE(Component.empty(), Component.empty());
 
     private final Component displayComponent;
