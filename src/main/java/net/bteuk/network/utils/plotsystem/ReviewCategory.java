1c74a4cb6c7602dcf79d8cc5f15c35b83238eeeb
ELgamer <laurens.wolfert@gmail.com>
2025-03-29 11:57:58 +0100

    Indicate which reviewing categories are required.

--- src/main/java/net/bteuk/network/utils/plotsystem/ReviewCategory.java
+++ src/main/java/net/bteuk/network/utils/plotsystem/ReviewCategory.java
@@ -5,17 +5,21 @@ import lombok.Getter;
 @Getter
 public enum ReviewCategory {
 
-    OUTLINES("Outlines"),
-    FEATURES("Features"),
-    ROOF("Roof"),
-    GARDEN("Garden"),
-    TEXTURES("Textures"),
-    DETAILS("Details"),
-    GENERAL("General");
+    GENERAL("General", false),
+
+    OUTLINES("Outlines", true),
+    FEATURES("Features", true),
+    ROOF("Roof", true),
+    GARDEN("Garden", true),
+    TEXTURES("Textures", true),
+    DETAILS("Details", true);
 
     private final String displayName;
 
-    ReviewCategory(String displayName) {
+    private final boolean required;
+
+    ReviewCategory(String displayName, boolean required) {
         this.displayName = displayName;
+        this.required = required;
     }
 }
