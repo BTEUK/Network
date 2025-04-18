70e0cae68d2c6f956ebc35b7be4716c6ce1a9d75
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-04-18 23:00:00 +0100

    added a building query that displays the ID of the building and the player who added it

--- src/main/java/net/bteuk/network/commands/Buildings.java
+++ src/main/java/net/bteuk/network/commands/Buildings.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import static net.bteuk.network.utils.Constants.SERVER_TYPE;
 
 public class Buildings extends AbstractCommand {
-    private static final Component ERROR = ChatUtils.error("/building add/show/count/delete/definition");
+    private static final Component ERROR = ChatUtils.error("/building add/show/count/delete/definition/query");
 
     private static final int SHOW_BUILDINGS_DURATION = 300;
 
@@ -29,7 +29,7 @@ public class Buildings extends AbstractCommand {
     public Buildings(Network instance) {
         super();
         this.instance = instance;
-        setTabCompleter(new FixedArgSelector(Arrays.asList("add", "show", "count", "delete", "definition"), 0));
+        setTabCompleter(new FixedArgSelector(Arrays.asList("add", "show", "count", "delete", "definition", "query"), 0));
     }
 
     @Override
@@ -71,12 +71,30 @@ public class Buildings extends AbstractCommand {
                         "A building is a structure that has walls on all sides, a roof, is larger than 2*3m and can be entered by a human. In other words use common sense. It " +
                                 "is up to you whether you count terraced houses as one or many buildings."));
                 break;
+            case "query":
+                queryBuilding(player);
+                break;
 
         }
 
     }
 
     private void deleteBuilding(Player player) {
+        Building minbuilding = getClosestBuilding(player);
+        if (minbuilding == null) {
+            return;
+        }
+        if (minbuilding.playerId().equals(player.getUniqueId().toString()) || player.hasPermission("network.buildings.delete")) {
+            instance.getGlobalSQL().deleteBuilding(minbuilding);
+            player.sendMessage(ChatUtils.success("Building deleted"));
+        } else {
+            player.sendMessage(ChatUtils.error("You don't have permission to delete this building"));
+
+        }
+
+    }
+
+    private Building getClosestBuilding(Player player) {
         ArrayList<Building> nearbyBuildings = getNearbyBuildings(player, 5);
         double minDist = 100;
         Building minbuilding = null;
@@ -89,16 +107,8 @@ public class Buildings extends AbstractCommand {
         }
         if (minbuilding == null) {
             player.sendMessage(ChatUtils.error("No buildings within 5 blocks"));
-            return;
         }
-        if (minbuilding.playerId().equals(player.getUniqueId().toString()) || player.hasPermission("network.buildings.delete")) {
-            instance.getGlobalSQL().deleteBuilding(minbuilding);
-            player.sendMessage(ChatUtils.success("Building deleted"));
-        } else {
-            player.sendMessage(ChatUtils.error("You don't have permission to delete this building"));
-
-        }
-
+        return minbuilding;
     }
 
     public static double getDist(Location l1, Location l2) {
@@ -132,6 +142,16 @@ public class Buildings extends AbstractCommand {
         player.sendMessage(ChatUtils.success("Building added at %s,%s", String.valueOf(l.getX()), String.valueOf(l.getZ())));
         int coordinateId = instance.getGlobalSQL().addCoordinate(l);
         instance.getGlobalSQL().update(String.format("INSERT INTO buildings (coordinate_id, player_id) VALUES (%d, '%s');", coordinateId, player.getUniqueId()));
+    }
+
+    private void queryBuilding(Player player) {
+        Building b = getClosestBuilding(player);
+        if (b == null) {
+            return;
+        }
+        String playerName = instance.getGlobalSQL().getString(String.format("SELECT name FROM player_data WHERE uuid='%s';", b.playerId()));
+        player.sendMessage(ChatUtils.success(String.format("Building ID: %d, Player: %s", b.buildingId(), playerName)));
+
     }
 
     private void showBuildings(Player player) {
